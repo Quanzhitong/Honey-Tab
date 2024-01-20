8724597d4eb9af69647eaf887f808091290cdadd
Quanzhitong <emqzt111@163.com>
2024-01-20 13:06:15 +0800

    hotfix-文案

--- src/introduce/App.tsx
+++ src/introduce/App.tsx
@@ -8,10 +8,11 @@ const App = () => (
         <div className="title">Honey Tab 首次使用说明</div>
         <div className="section">功能：</div>
         <div className="feature">1、列表展示所有标签页，支持拖动、删除。</div>
-        <div className="feature">2、标签页分组规则：</div>
-        <div className="sub-feature">2.1 支持设定分组标签页的最低数量</div>
-        <div className="sub-feature">2.2 支持按域名分组等级分组</div>
-        <div className="sub-feature">2.3 支持设置是否展开全部分组</div>
+        <div className="feature">2、标签页可配置分组规则：</div>
+        <div className="sub-feature">2.1 支持设置标签页生效范围</div>
+        <div className="sub-feature"> 2.2 支持设定分组标签页的最低数量</div>
+        <div className="sub-feature">2.3 支持设定域名匹配等级</div>
+        <div className="sub-feature">2.4 支持设置是否展开全部分组</div>
         <div className="feature">3、支持多个chrome浏览器窗口一键合并</div>
         <div className="section">Tips：</div>
         <div className="tips">1、首次使用可按下面步骤，将插件“钉”在插件栏</div>
